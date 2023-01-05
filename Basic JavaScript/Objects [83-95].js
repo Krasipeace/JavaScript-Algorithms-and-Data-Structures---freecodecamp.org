219f7818bd6a79761848e7149d19d7888f695f98
Krasimir Dramaliev <krasipeace@abv.bg>
2023-01-05 23:56:46 +0200

    define objects

--- Basic JavaScript/Objects [83-95].js
+++ Basic JavaScript/Objects [83-95].js
@@ -48,29 +48,29 @@ const testObj3 = {
 
 //87 Updating Object Properties
 // Setup
-const myDog = {
+const myDog2 = {
     "name": "Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
-  myDog.name = "Happy Coder";
+  myDog2.name = "Happy Coder";
   // Only change code below this line
 
   
 // 88 Add New Properties to a JavaScript Object
-const myDog = {
+const myDog3 = {
     "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
-  myDog.bark = "woof";
+  myDog3.bark = "woof";
 
   
 //89 Delete Properties from a JavaScript Object
 // Setup
-const myDog = {
+const myDog4 = {
     "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
@@ -79,7 +79,7 @@ const myDog = {
   };
   
   // Only change code below this line
-  delete myDog.tails;
+  delete myDog4.tails;
 
 
 //90 Using Objects for Lookups
@@ -117,3 +117,105 @@ function checkObj(obj, checkProp) {
 
 
 //92 Manipulating Complex Objects
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Battle Beast",
+    "title": "Ciscus of Doom",
+    "release_year": 2022,
+    "formats": [
+      "MP3",
+      "CD",
+      "Spotify"
+    ]
+  }
+];
+
+
+//93 Accessing Nested Objects
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+//94 Accessing Nested Arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+
+//95 Record Collection 
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != 'tracks' && value != "") {
+    records[id][prop] = value;
+  } else if (prop == "tracks" && records[id].hasOwnProperty("tracks") == false) {
+    records[id][prop] = [value];
+  } else if (prop == "tracks" && value != "") {
+    records[id][prop].push(value);
+  } else if (value == "") {
+    delete records[id][prop];
+  }
+  
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
