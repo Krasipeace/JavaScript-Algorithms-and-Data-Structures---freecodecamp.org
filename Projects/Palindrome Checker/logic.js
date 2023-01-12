0234a4798ebc3dd4c7a00b233a2eb223f3aea391
Krasimir Dramaliev <krasipeace@abv.bg>
2023-01-12 19:16:51 +0200

    project 1

---
+++ Projects/Palindrome Checker/logic.js
@@ -0,0 +1,31 @@
+function palindrome(str) { 
+    let regex = /[\W_]/g; 
+    
+    let inputText = str.toLowerCase().replace(regex, ''); 
+    let reversedText = inputText.split('').reverse().join('');   
+    
+    return reversedText === inputText; 
+}
+
+let submitButtonEvent = document.getElementById("submitButton");
+
+submitButtonEvent.addEventListener("click", function() { 
+    'use strict'; 
+
+    var value = document.getElementById("inputString").value; 
+    var notification = document.getElementById("notification"); 
+    var displayWord = document.getElementById("displayWord");
+
+    if(palindrome(value)) { 
+    notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is a Palindrome!`;    
+    displayWord.innerHTML = `Forwards: ` + value.toLowerCase().replace(/[\W_]/g, '') + "<br>" + `Backwards: ` + value.toLowerCase().replace(/[\W_]/g, '').split('').reverse().join('');    
+    } else { 
+    notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is NOT a palindrome!`; 
+    displayWord.innerHTML = `Forwards: ` + value.toLowerCase().replace(/[\W_]/g, '') + "<br>" + `Backwards: ` + value.toLowerCase().replace(/[\W_]/g, '').split('').reverse().join('');  
+    }
+}); 
+
+
+// Tests
+//console.log(palindrome("eye")); // true
+//console.log(palindrome("KRAS")); // false  
