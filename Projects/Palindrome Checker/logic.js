a23fb9f4e9f77808319f9bc31439c9dc53231d62
Krasimir Dramaliev <krasipeace@abv.bg>
2023-01-20 10:44:35 +0200

    update

--- Projects/Palindrome Checker/logic.js
+++ Projects/Palindrome Checker/logic.js
@@ -14,13 +14,11 @@ submitButtonEvent.addEventListener("click", function() {
 
     let value = document.getElementById("inputString").value; 
     let notification = document.getElementById("notification"); 
-    let displayWord = document.getElementById("displayWord");
 
     if(palindrome(value)) { 
-    notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is a palindrome!`;    
-    displayWord.innerHTML = `Forwards: ` + value.toLowerCase().replace(/[\W_]/g, '') + "<br>" + `Backwards: ` + value.toLowerCase().replace(/[\W_]/g, '').split('').reverse().join('');    
-    } else { 
-    notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is not a palindrome!`; 
-    displayWord.innerHTML = `Forwards: ` + value.toLowerCase().replace(/[\W_]/g, '') + "<br>" + `Backwards: ` + value.toLowerCase().replace(/[\W_]/g, '').split('').reverse().join('');  
+        notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is a palindrome!`; 
+    } 
+    else { 
+        notification.innerHTML = "\"" + value.toString() + "\"" + ` ` + `is not a palindrome!`;
     }
 }); 
--- Projects/Telephone Number Validator/simple version.js
+++ Projects/Telephone Number Validator/simple version.js
@@ -2,12 +2,12 @@ function telephoneCheck(str) {
     let regexOne = /^(1\s?)?\d{3}([-\s]?)\d{3}\2\d{4}$/;
     let regexTwo = /^(1\s?)?\(\d{3}\)\s?\d{3}[-\s]?\d{4}$/;
 
-    if (regexOne.test(str)) {
-        return true;
-    } 
-    else {
-        return regexTwo.test(str) ? true : false
+    if (!regexOne.test(str)) {
+        return regexTwo.test(str);
     }
+
+    return true;
 }
   
 console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("(555)-555-5555"));
